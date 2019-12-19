32d419891c10808679f3f55649a665de6b34918d
wesley perez <wesley.m.perez@gmail.com>
2019-12-19 02:46:21 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  array.each do |arr|
+    arr ** 2
+    while counter < array do
+      puts array[counter]
+    counter += 1
+  end
 end
+essay_grade = [1,3,2,5,8]
+square_array(essay_grade,2)
